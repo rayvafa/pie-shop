2954f56a18d2bbecbc509e53b9039319f785c3ec
Ray Vafa <vafatabar@gmail.com>
2017-08-13 19:03:20 +1000

    Create a contact page

--- PieShop/Controllers/PieController.cs
+++ PieShop/Controllers/PieController.cs
@@ -39,13 +39,6 @@ namespace PieShop.Controllers
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View();
