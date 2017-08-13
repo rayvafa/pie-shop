7d42c251a9a9991cd98d6ed8d798868cef8be7e3
Ray Vafa <vafatabar@gmail.com>
2017-08-13 17:46:04 +1000

    Added ShoppingCartItem migration files

--- PieShop/Migrations/20170813074454_AddShoppingCartItem.Designer.cs
+++ PieShop/Migrations/20170813074454_AddShoppingCartItem.Designer.cs
@@ -8,9 +8,10 @@ using PieShop.Models;
 namespace PieShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170813074454_AddShoppingCartItem")]
+    partial class AddShoppingCartItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
