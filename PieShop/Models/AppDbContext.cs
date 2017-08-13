a20237e7fc7a033d7c774652513b4e4da2619783
Ray Vafa <vafatabar@gmail.com>
2017-08-13 17:42:16 +1000

    Add shopping card item model

--- PieShop/Models/AppDbContext.cs
+++ PieShop/Models/AppDbContext.cs
@@ -15,5 +15,7 @@ namespace PieShop.Models
         public DbSet<Category> Categories { get; set; }
 
         public DbSet<Pie> Pies { get; set; }
+
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
